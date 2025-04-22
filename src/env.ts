3a491518b1c193c1e8fcc78cd9db2c7b7b98bd3e
Hugo Cruz <hugocruzlfc@gmail.com>
2025-04-22 17:05:11 +0200

    feat: add main prooject config

---
+++ src/env.ts
@@ -0,0 +1,16 @@
+import { createEnv } from "@t3-oss/env-nextjs";
+import { z } from "zod";
+
+export const env = createEnv({
+  server: {
+    BACKEND_URL: z.string().url(),
+  },
+  client: {
+    NEXT_PUBLIC_URL: z.string().min(1),
+  },
+
+  runtimeEnv: {
+    BACKEND_URL: process.env.BACKEND_URL,
+    NEXT_PUBLIC_URL: process.env.NEXT_PUBLIC_URL,
+  },
+});
